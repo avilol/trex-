9f8ed83e0ca32482fca518d520256c7717a8e266
pppaimonster <92992277+pppaimonster@users.noreply.github.com>
2021-10-28 16:56:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -100,7 +100,7 @@ function draw() {
     ground.velocityX = -(6 + 3* score/100)
     //scoring
     score = score + Math.round(getFrameRate()/60);
-    if((touches.length> 0 keyDown("SPACE")) && trex.y >= height-120){
+    if((touches.length> 0 || keyDown("SPACE")) && trex.y >= height-120){
       jumpSound.play ()
       trex.velocityY=-10;
       touches = [];
